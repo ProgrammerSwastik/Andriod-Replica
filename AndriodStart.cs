6156c6470fae9dfee68ec57eba89bdccdda655e6
Swastik Bhattacharjee <73767874+SwastikBhattacharjee@users.noreply.github.com>
2021-01-08 09:23:54 +0530

    Add files via upload

---
+++ AndriodStart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Andriod_Emulator
+{
+    public partial class AndriodStart : Form
+    {
+        public AndriodStart()
+        {
+            InitializeComponent();
+
+            /*Form LockScreen = new LockScreen();
+
+            Thread.Sleep(3000);
+            LockScreen.Show();
+            this.Hide();*/
+
+        }
+
+    }
+}
---
+++ Control.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Andriod_Emulator
+{
+    public partial class Control : Form
+    {
+        public Control()
+        {
+            InitializeComponent();
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+            FullBattery.Hide();
+            NearlyFullBattery.Hide();
+            LowBattery.Hide();
+            HalfBattery.Hide();
+            PowerOff.Enabled = false;
+            WebBrowsersFolder.Enabled = false;
+            Home.Enabled = false;
+            Lock.Enabled = false;
+            FullBattery.Enabled = false;
+            Charging.Enabled = false;
+            FullBattery.Enabled = false;
+            NearlyFullBattery.Enabled = false;
+            LowBattery.Enabled = false;
+            HalfBattery.Enabled = false;
+        }
+
+        private void PowerOn_Click(object sender, EventArgs e)
+        {
+            Form AndriodStart = new AndriodStart();
+            AndriodStart.Show();
+
+            PowerOff.Enabled = true;
+            WebBrowsersFolder.Enabled = true;
+            Home.Enabled = true;
+            Charging.Hide();
+            FullBattery.Show();
+
+            Thread.Sleep(3600000);
+            FullBattery.Hide();
+            NearlyFullBattery.Show();
+
+            Thread.Sleep(7200000);
+            NearlyFullBattery.Hide();
+            HalfBattery.Show();
+
+            Thread.Sleep(10800000);
+            HalfBattery.Hide();
+            LowBattery.Show();
+
+
+
+        }
+    }
+}
